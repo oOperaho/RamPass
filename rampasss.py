3e288fc4058d7711455c249f2a6bcb656145da60
oOperaho <emanuelllevi7770@gmail.com>
2021-08-07 14:51:56 -0300

    →

--- rampasss.py
+++ rampasss.py
@@ -1,6 +1,7 @@
 import sys
 import random
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 
 
 def mix(string):
@@ -9,42 +10,48 @@ def mix(string):
     return ''.join(alg_list)
 
 
+class MainWidget(QMainWindow):
+    def __init__(self):
+        super(MainWidget, self).__init__()
+        self.setStyleSheet("background-color: #161B22;")
+        self.setGeometry(100, 100, 520, 400)
+        self.setWindowTitle("RamPass")
+        self.startui()
+
+    def startui(self):
+        self.label_pass = QtWidgets.QLabel(self)
+        self.label_pass.setText("      ")
+        self.label_pass.move(215, 150)
+        self.label_pass.setStyleSheet("background-color: #FFFFFF;")
+
+        self.go_button = QPushButton(self)
+        self.go_button.setText("Go")
+        self.go_button.move(215, 190)
+        self.go_button.setStyleSheet("background-color: #4DA9F9;")
+        self.go_button.clicked.connect(self.go_button_clicked)
+
+    def go_button_clicked(self):
+        fnum1 = random.randint(0, 9)
+        fnum2 = random.randint(0, 9)
+        uletter1 = chr(random.randint(65, 90))
+        uletter2 = chr(random.randint(65, 90))
+        lletter1 = chr(random.randint(97, 122))
+        lletter2 = chr(random.randint(97, 122))
+        spcchar1 = chr(random.randint(33, 64))
+        spcchar2 = chr(random.randint(33, 64))
+
+        final_password = uletter1 + uletter2 + str(fnum1) + str(fnum2) + lletter1 + lletter2 + spcchar1 + spcchar2
+        final_password = mix(final_password)
+
+        self.label_pass.setText(final_password)
+
+
 def window():
     qt = QApplication(sys.argv)
-    widget = QWidget()
-
-    genpass_label = QLabel(widget)
-    genpass_label.setText("Generate Password")
-
-    go_button = QPushButton(widget)
-    go_button.setText("Go")
-    go_button.move(215, 190)
-    go_button.setStyleSheet("background-color: #4DA9F9;")
-    go_button.clicked.connect(go_button_clicked)
-
+    widget = MainWidget()
     qt.setStyle("Fusion")
-    widget.setStyleSheet("background-color: #161B22;")
-    widget.setGeometry(100, 100, 520, 400)
-    widget.setWindowTitle("RamPass")
     widget.show()
     sys.exit(qt.exec_())
-
-
-def go_button_clicked():
-
-    fnum1 = random.randint(0, 9)
-    fnum2 = random.randint(0, 9)
-    uletter1 = chr(random.randint(65, 90))
-    uletter2 = chr(random.randint(65, 90))
-    lletter1 = chr(random.randint(97, 122))
-    lletter2 = chr(random.randint(97, 122))
-    spcchar1 = chr(random.randint(33, 64))
-    spcchar2 = chr(random.randint(33, 64))
-
-    final_password = uletter1 + uletter2 + str(fnum1) + str(fnum2) + lletter1 + lletter2 + spcchar1 + spcchar2
-    final_password = mix(final_password)
-
-    print(final_password)
 
 
 window()
