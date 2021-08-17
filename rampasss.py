a07f9c2d877283072295ffc1a78f0b20c86f93eb
oOperaho <emanuelllevi7770@gmail.com>
2021-08-16 23:09:13 -0300

    → new label

--- rampasss.py
+++ rampasss.py
@@ -30,7 +30,7 @@ class MainWidget(QMainWindow):
         self.genpass.setStyleSheet("""background-color: #161B22; color: #65F791;""")
 
         self.label_pass.setText("")
-        self.label_pass.move(215, 150)
+        self.label_pass.move(215, 148)
         self.label_pass.setFont(QFont("Times New Roman", 10))
         self.label_pass.setStyleSheet("""background-color: #161B22; color: #FFFFFF;""")
 
