f6b77cf9e00f1aa93d832dd88279b5d192c6aaff
oOperaho <emanuelllevi7770@gmail.com>
2021-08-16 23:48:41 -0300

    → new label

--- rampasss.py
+++ rampasss.py
@@ -59,7 +59,7 @@ class MainWidget(QMainWindow):
 def window():
     qt = QApplication(sys.argv)
     widget = MainWidget()
-    qt.setStyle("QtCurve")
+    qt.setStyle("Breeze")
     widget.show()
     sys.exit(qt.exec_())
 
