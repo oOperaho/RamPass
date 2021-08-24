4190372739bbeb70eed08a92ef324bcb5692441e
oOperaho <emanuelllevi7770@gmail.com>
2021-08-23 23:44:34 -0300

    → Button to browser

--- rampasss.py
+++ rampasss.py
@@ -61,8 +61,8 @@ class MainWidget(QMainWindow):
         self.label_pass.setText(" " * 8 + final_password)
 
 
-def open_repo(self):
-    self.webbrowser.open("https://github.com/oOperaho/RamPass")
+    def open_repo(self):
+        self.webbrowser.open("https://github.com/oOperaho/RamPass")
 
 
 def window():
