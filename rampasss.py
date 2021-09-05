b0664ad6bf4b158c86287edd3f4ac4cd3cfad9ef
oOperaho <emanuelllevi7770@gmail.com>
2021-09-05 16:21:28 -0300

    →

--- rampasss.py
+++ rampasss.py
@@ -44,7 +44,7 @@ class MainWidget(QMainWindow):
         self.repo_button.setText("•")
         self.repo_button.setGeometry(10, 10, 12, 12)
         self.repo_button.setStyleSheet("background-color: #161B22; color: #65F791")
-        self.repo_button.clicked.connect(self.webbrowser.open_new("https://github.com/oOperaho/RamPass"))
+        self.repo_button.clicked.connect(self.open_repo)
 
     def go_button_clicked(self):
         fnum1 = random.randint(0, 9)
@@ -61,8 +61,8 @@ class MainWidget(QMainWindow):
 
         self.label_pass.setText(" " * 8 + final_password)
 
-    # def open_repo(self):
-    #     self.webbrowser.open_new("https://github.com/oOperaho/RamPass")
+    def open_repo(self):
+        self.webbrowser.open_new("https://github.com/oOperaho/RamPass")
 
 
 def window():
