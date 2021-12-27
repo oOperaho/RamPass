0b35d796fff981fbe9ebd078d934ad69cdcff39a
oOperaho <emanuelllevi7770@gmail.com>
2021-12-26 22:12:08 -0300

    >

--- rampasss.py
+++ rampasss.py
@@ -49,7 +49,7 @@ class MainWidget(QMainWindow):
         self.repo_button.clicked.connect(self.open_repo)
 
         self.copy_text.setText("C")
-        self.copy_text.setGeometry(20, 20, 20, 20)
+        self.copy_text.setGeometry(200, 200, 200, 200)
         self.copy_text.setStyleSheet("background-color: #161B22; color: white")
 
     def go_button_clicked(self):
