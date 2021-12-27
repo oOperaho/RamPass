dd26267414fa716e333eeb1e19a21f6145e8bdc8
oOperaho <emanuelllevi7770@gmail.com>
2021-12-26 22:59:21 -0300

    →

--- rampasss.py
+++ rampasss.py
@@ -69,6 +69,7 @@ class MainWidget(QMainWindow):
         final_password = mix(final_password)
 
         self.label_pass.setText(" " * 8 + final_password)
+        print(final_password)
 
     def open_repo(self):
         webbrowser.open_new(self.url)
